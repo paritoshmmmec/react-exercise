45fc9336552e4e20509a0d8b800d536b2fa81f54
paritosh baghel <paritoshmmmec@gmail.com>
2019-08-10 16:53:06 -0400

    Update ObjectHooks.js

--- hooks/src/ObjectHooksApp/ObjectHooks.js
+++ hooks/src/ObjectHooksApp/ObjectHooks.js
@@ -2,18 +2,39 @@ import React, { useState, Fragment } from 'react'
 
 const ObjectHooks = () => {
 
-    const [appState, setAppState] = useState({
+    const initialState = {
         firstName: 'Emma',
         lastName: 'Weikend',
-        isEnrolled: true,
+        isEnrolled: false,
         tasks: []
-    })
+    }
+
+    const [appState, setAppState] = useState(initialState)
 
 
     return (
         <Fragment>
             <div>
-                <button onClick={(e) => { setAppState({ ...appState, isEnrolled: !appState.isEnrolled }) }} >Change Customer Enrolled Status</button>
+                <input type="text" value={appState.firstName}
+                    onChange={(e) => { setAppState({ ...appState, firstName: e.target.value }) }} />
+
+            </div>
+            <div>
+                <input type="checkbox" checked={appState.isEnrolled}
+                    onChange={(e) => { setAppState({ ...appState, isEnrolled: !appState.isEnrolled }) }} >
+                </input> Change Customer Enrolled Status
+            </div>
+            <div>
+                <input type="button" value="Reset Customer"
+                    onClick={(e) => { setAppState({ ...initialState }) }} />
+
+            </div>
+
+            <div>
+                {appState.isEnrolled.toString()}
+            </div>
+            <div>
+                {appState.firstName}
             </div>
         </Fragment>
     )
